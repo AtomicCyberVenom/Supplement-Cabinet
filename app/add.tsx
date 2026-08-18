e01b64d1351169a7aa7a12e7dd228f05af1c5ec2
Bonnie <bonniechiu527@gmail.com>
2026-08-18 14:35:46 +1000

    Add search.tsx & add.tsx

---
+++ app/add.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+
+export default function AddSupplement() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Add Supplement</Text>
+    </View>
+  );
+}
